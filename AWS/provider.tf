2da19597839b3745682e9c7dc8183ecb7f50e530
prashanthmanohar <prashanthnaikaar@gmail.com>
2019-10-29 13:33:14 +0530

    Removing AWS credentials

--- AWS/provider.tf
+++ AWS/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 	region = "${var.aws_region}"
 	#profile = "$var.aws_profile}"
-	access_key = "AKIAZ4XPBOBOTCV5KNWC"
-	secret_key = "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"
+	access_key = "***"
+	secret_key = "****"
 }
