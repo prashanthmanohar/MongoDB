d41d4205b90bc742e558b55708c05f5b7f7e11dc
prashanthmanohar <prashanthnaikaar@gmail.com>
2019-11-11 07:11:54 -0800

    Update provider.tf

--- AWS/provider.tf
+++ AWS/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "${var.aws_region}"
   #profile = "$var.aws_profile}"
-  access_key = "AKIAZ4XPBOBO5LVA3AJN"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
